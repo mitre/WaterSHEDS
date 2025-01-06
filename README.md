2fe5bd1441a7768f6e9fe8f2df47a9ea155ba6b1
Connor McMillan <20669370+bmcmillan2@users.noreply.github.com>
2025-01-06 12:03:47 -0500

    initial commit of files

---
+++ README.md
@@ -0,0 +1,106 @@
+## WaterSHEDs
+
+### Modeling Surface Water PFAS Vulnerability 
+##
+#### NOTICE: © 2024 The MITRE Corporation. All Rights Reserved. Approved for Public Release; Distribution Unlimited. Public Release Case Number 24-3501.
+ 
+#### NOTICE: MITRE hereby grants express written permission to use, reproduce, distribute, modify, and otherwise leverage this software to the extent permitted by the licensed terms provided in the LICENSE.md file included with this project.
+
+
+##
+
+### Requirements to Run 
+1. The Esri provided Python Environment 
+2. A jupyter notebook server 
+3. A single directory that contains: 
+   1. The WaterSHEDs notebook
+   2. All data required to run (more information below)
+4. An EPSG code corresponding to the UTM Zone for your AOI 
+   - This is used in the code to convert Vector Outputs to a Raster
+   - A supported list of EPSG codes can be found [here](https://pro.arcgis.com/en/pro-app/latest/arcpy/classes/pdf/projected_coordinate_systems.pdf)
+5. A File Geodatabase Workspace
+   - This geodatabase will be where your 1) base data is stored and 2) your results are written to
+   - Note: The geodatabase should to include all of the data referenced in this section. 
+
+### Data
+6. A polygon denoting your Area of Interest (AOI) 
+   - This polygon should be included in your File Geodatabase Workspace
+7. Data for your AOI from the National Hydrography Dataset 
+   - This dataset is used to identify potential downstream vulnerabilities from hazard sources. The [NHDPlus HR](https://hydro.nationalmap.gov/arcgis/rest/services/NHDPlus_HR/MapServer) is a national, geospatial model of the flow of water across the landscape and through the stream network. The NHDPlus HR is built using the National Hydrography Dataset High Resolution data at 1:24,000 scale or better, the 1/3 arc-second (10 meter ground spacing) 3D Elevation Program data, and the nationally complete Watershed Boundary Dataset.  
+       - [Access data from the USGS National Map here](https://www.epa.gov/waterdata/get-nhdplus-national-hydrography-dataset-plus-data#NHDPlusV2Map)
+       - [Access data from staged ftp site here](https://prd-tnm.s3.amazonaws.com/index.html?prefix=StagedProducts/Hydrography/NHDPlusHR/Beta/GDB/)
+
+8.  A copy of the EPA's Facility Registry System (FRS). This data contains a file that has all of the NAICS codes for facilities within the system.  It can be downloaded here:  [EPA State Combined CSV Download Files](https://ordsext.epa.gov/FLA/www3/state_files/national_combined.zip). 
+    -  This data is used to calculte a preliminary vulnerability based on teh industry type. 
+    -  The file you want to extract from this ZIP is the`"NATIONAL_NAICS_File.csv`
+
+9.  A copy of the NHDPlusV21 catchment data from the EPA to include: 
+   -  Download Option: [NHPPlusV2](https://www.epa.gov/waterdata/get-nhdplus-national-hydrography-dataset-plus-data#v2datamap) 
+      - You will need to identify your region and download the `NHDPlusV21_{RegionName}_NHDPlusCatchment.7z` file
+      - These files are needed to aggregate the results and configure summary attributes. 
+   - There is also an [ArcGIS Feature Server](https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/NHDPlusV21/FeatureServer) that contains the data. 
+   - The shapefile denoting the catchment region will need to be imported into the File Geodatabase specified in requirement 4
+
+10. PFAS Hazard Source Data
+    - This notebook will download this data for you and you and you do not need to indvidually download them. That said, for awareness, the layers are listed below.
+    1.  [DoD Site Boundaries](https://services7.arcgis.com/n1YM8pTrFmm7L4hs/arcgis/rest/services/mirta/FeatureServer/1)
+    2.  [Fire Stations](https://carto.nationalmap.gov/arcgis/rest/services/structures/MapServer/51)
+    3.  [Oil and Natural Gas Wells](https://services7.arcgis.com/FGr1D95XCGALKXqM/ArcGIS/rest/services/Oil_Wells/FeatureServer/0)
+    4.  [Solid Waste Landfill Facilities](https://services.arcgis.com/cJ9YHowT8TU7DUyn/arcgis/rest/services/EPA_Disaster_Debris_Recovery_Data/FeatureServer/0)
+    5.  [Runways](https://services6.arcgis.com/ssFJjBXIUyZDrSYZ/arcgis/rest/services/Runways/FeatureServer/0)
+    6.  [NPDES Facilities](https://services.arcgis.com/cJ9YHowT8TU7DUyn/arcgis/rest/services/oeca__echo__npdes_facilities_outfalls/FeatureServer/0)
+    7.  [Facilities of Interest](https://services.arcgis.com/cJ9YHowT8TU7DUyn/ArcGIS/rest/services/FRS_INTERESTS/FeatureServer/0)
+    8.  [DoD Formerly Used Defense Sites](https://services7.arcgis.com/n1YM8pTrFmm7L4hs/ArcGIS/rest/services/FUDS_property_areas/FeatureServer/0)
+    - **Note**: This notebook was written with these as the input layers.  With that in mind, many of the layer names in the notebook refer to the above. If you have additional layers you wish to download, or different layers all together, you will need to identify and update those references below. 
+
+##
+### Steps to Run 
+**Note**: It is reccomended that you do NOT run this notebook from within ArcGIS Pro. 
+1. In order to run the code, first open the python command prompt that comes with ArcGIS Pro
+   1. In the Windows start menu, navigate to the ArcGIS folder and selct the "Python Command Prompt" option
+
+2. One the command prompt is open, type the following: `jupyter notebook` 
+   - Note: If this is your first time running the command, you will not have a `jupyter_notebook_config.py` file.  This means your notebook will default to and display all files in the ArcGIS Pro python directory.  To change this behavior: 
+     -  Shut down the existing jupyter notebook by pressing `CTRL + c` and select `y` when prompted
+     -  In the same terminal, type `jupyter notebook --generate-config` 
+        -  This will create a config file for you
+     -  Navigate to the directory where the config file was created and open the `jupyter_notebook_config.py` file
+     -  Search for and update the following to your desired directory: `c.NotebookApp.notebook_dir`
+     -  Save the file and re-run the `jupyter notebook` command
+
+3. In the resulting notebook server, navigate to where your copy of the WaterSHEDs notebook and all data exists
+   
+4. Run the initial import steps and, when prompted, answer the following: 
+   1. What is the file path of your File Geodatabase workspace? 
+   2. Please enter the name of your AOI HUC boundary that is located in your FGDB workspace
+   3. Please enter the path to the input NHD Flowline Flie
+   4. Please enter the path to the NHDPlus Flowline VAA table
+   5. Please enter the path to the NHDPlus EROMMA file
+   6. Please enter the path to the NHDPlus Catchment layer
+   7. Please enter the path to the NHDPlus Hydro Geometric Network file
+      1. **Note**: This is used to create and enable the trace network. The tool used in ArcPy to preform this PERMANENTLY MODIFIES the input data. The cell, named "Create and Enable Trace Network", can only be run ONCE with the HYDRO NET input layer. 
+   8. Please enter the EPSG code that corresponds to the UTM Zone for your AOI
+      1. Please see the following for reference [here](https://pro.arcgis.com/en/pro-app/latest/arcpy/classes/pdf/projected_coordinate_systems.pdf)
+         - It is reccomended you use a NAD_1983_11 based projection. These can be found starting at the bottom of Page 78
+   9.  Please enter the full path to the CSV that contains the NAICS codes for facilities in the FRS
+   10. Specify a short name to differentiate between outputs 
+##
+### While Running the Notebook
+- There are instances where user input will still be required to help differentiate outputs. These occur: 
+  - Under Vulnerability Modeling when calulating Distance Decay, users will be required to specify a unique File Geodatabase Table name to store results.
+  - When calculating the weighted sum, users will be required to enter weights for the various layers.  The default values used by the MITRE team are: 
+  
+     -    DoD Facilities: 0.17
+     -    Facilities of Interest (FOI) - 100 = 0.15
+     -    Facilities of Interest (FOI) - 75 = 0.13
+     -    Facilities of Interest (FOI) - 50 = 0.11
+     -    Formerly Used Defense Sites (FUDS) = 0.08
+     -    Runways = 0.08
+     -    Landfills = 0.08 
+     -    Facilities of Interest (FOI) - 25 = 0.06 
+     -    NPDES Permitted Facilities = 0.06 
+     -    Firestations = 0.03
+     -    Oiland Gas Facilities = 0.03
+     -    Facilities of Interest (FOI) - 1 = 0.02
+     
+     
---
+++ hazard_processing_mp.py
@@ -0,0 +1,267 @@
+'''
+Multiprocessing approach to calculating all of the downstream flow lines. Please do not run this without talking to Ryan Hollins or Connor McMillan as there are precautions that should be taken
+'''
+
+import arcpy
+import shutil 
+import os 
+from multiprocessing import Pool, Manager,cpu_count
+from datetime import datetime 
+import uuid
+import time 
+import stat 
+import logging
+
+ws = input("What is the file path of your File Geodatabase workspace: ")
+
+tmp_dir = os.path.join(os.path.dirname(ws),"temp_dir")
+
+output_trace_dir = os.path.join(os.path.join(os.path.dirname(ws), "trace_outputs"))
+arcpy.env.workspace=ws
+arcpy.env.overwriteOutput=True
+
+## USER INPUT -- Specify the AOI HUC boundary, located in the FGDB workspace, 
+## for clipping data and setting extent:
+AOI = input('Please enter the name of your AOI HUC boundary that is located in your FGDB workspace: ')
+
+## USER INPUT -- Specify the file path of the NHDPlus Hydro Geometric Network:
+in_geometric_network=input("Please enter the path to the NHDPlus Hydro Geometric Network file: ")
+
+## User Input -- Specify the number of processors to use (will adivse if it's greater than cpu_count)
+print(f'Your machine has {cpu_count()} processors available.  How many do you wish to use? ')
+process_count_to_use = input()
+
+##USER INPUT -- Specify the UTM Zone for your AOI -- Used when converting Vectors to Raster Output 
+## Please see the README for a listing of acceptable codes.
+utm_output_coordinate_system_number=32618
+logFile = os.path.join(os.getcwd(), os.path.basename(ws)+ f"_{datetime.now().strftime('%Y%m%d%H%M%S')}.log")
+logging.basicConfig(filename = logFile, filemode = 'a', format='%(asctime)s - %(message)s', level = logging.DEBUG)
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(message)s')
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+def create_fgdb(directory:str, name:str):
+    ''' If one does not already exist, this will create the FGDB to store the results 
+
+    Parameters
+    ----------
+        directory: str
+            name of the base directory to create the FGDB
+        name: str 
+            the name of the output FGDB
+    Returns
+    ----------    
+    str
+        Full path to the created, or already existing FGDB
+    '''
+
+    outName = f"{name}.gdb"
+
+    outPath = os.path.join(directory, outName)
+    if os.path.exists(outPath):
+        return outPath
+    else:
+        try:
+            # print(f"Creating File GeoDatabase Named {name} ")
+
+            arcpy.CreateFileGDB_management(directory, outName, "CURRENT")
+        except Exception as e:
+            print(f"[!!!!!!!!!!!!!!!!!!!!!!!!!! Error creating FileGeoDatabase ---> {e} -> !!!!!!!!!!!!!!!!!!!!!!!!!!]")
+        #else:
+        #    print(f"File GeoDatabase created and is available at {outPath}")
+
+    return outPath
+
+def change_file_times(in_dir):
+    current_time = time.time()
+
+    os.utime(in_dir,(current_time,current_time))
+    for x in os.listdir(in_dir): 
+        os.utime(os.path.join(in_dir,x),(current_time,current_time))
+
+def delete(in_dir):
+    '''
+    Will loop through the temporary geodatabase and attempt to delete any files that were created. 
+
+    '''
+    for x in os.listdir(in_dir): 
+        if ".sr.lock" not in x: 
+            try:
+                os.remove(os.path.join(in_dir,x))  
+            # shutil.rmtree(in_dir, onerror=on_rm_error)
+            except Exception as e: 
+                pass   
+
+def preform_hazard_trace(inputs):
+    '''
+    Preforms the downstream traces
+    inputs: list 
+        Index 0: original directory containing the hydronet files
+        Index 1: input feature class to use in the calculation 
+        Index 2: Current workspace (what `arcpy.env.workspace` is set to)
+        Index 3: 
+    
+    '''
+    
+    run_uuid = uuid.uuid4().hex
+    og_hydronet_dir = os.path.dirname(os.path.dirname(inputs[0]))
+
+    new_hydronet_dir = os.path.join(tmp_dir,f'{os.path.basename(og_hydronet_dir).split(".gdb")[0]}_{run_uuid}.gdb')
+
+    shutil.copytree(og_hydronet_dir, new_hydronet_dir)
+    os.chmod(new_hydronet_dir,stat.S_IWRITE)
+    change_file_times(new_hydronet_dir)
+
+    HYDRO_NET_TRACE_Network = os.path.join(f'{new_hydronet_dir}','Hydrography','HydroNet_Trace')
+    fcLine = inputs[1]
+    # print(fcLine)
+    ws = inputs[2]
+    try: 
+        print(f'Processing {datetime.now()} - {fcLine} ')
+        trace_output_gdb = create_fgdb(output_trace_dir, f'trace_{run_uuid}')
+
+    #     ## Copying the input fcLine file so we hopefully don't get any error messages
+        arcpy.management.CopyFeatures(fcLine, os.path.join(trace_output_gdb, fcLine), '', None, None, None)
+        
+        fcLine = os.path.join(trace_output_gdb, fcLine)
+
+        field_names_fcLine = []
+        field_names = []
+        field_names_fcLine = [f.name for f in arcpy.ListFields(fcLine, "Starting_NHDPlusID")]
+            
+        traceGroupName = fcLine+"_TraceGroup"
+        outName="tempTrace_"+os.path.basename(fcLine)
+        output_file = os.path.join(trace_output_gdb, outName) 
+        
+        # Compute Trace without aggregation, which results in a selection. 
+        arcpy.tn.Trace(HYDRO_NET_TRACE_Network, "DOWNSTREAM", fcLine, 
+                    '', "NO_DIRECTION", '', "EXCLUDE_BARRIERS", "DO_NOT_VALIDATE_CONSISTENCY", 
+                    "DO_NOT_IGNORE_BARRIERS_AT_STARTING_POINTS", "IGNORE_INDETERMINATE_FLOW", None, None, 
+                    "BOTH_JUNCTIONS_AND_EDGES", None, None, "NETWORK_LAYERS;SELECTION", "NEW_SELECTION", 
+                    "CLEAR_ALL_PREVIOUS_TRACE_RESULTS", '', "Trace_Results_Aggregated_Points", 
+                    "Trace_Results_Aggregated_Lines", traceGroupName, "DO_NOT_USE_TRACE_CONFIGURATION", '', None)
+        
+        copyTraceGroupNameFlowline = traceGroupName+"\\"+"NHDFlowline"
+
+        # Copy/save the selected trace to the output file location and join the 
+        # Starting_NHDPlusID field to each output.
+        try: 
+            arcpy.management.CopyFeatures(copyTraceGroupNameFlowline, output_file, '', None, None, None)  
+        except Exception as e: 
+            print('exception on copy features', e)
+
+        arcpy.JoinField_management(output_file,"NHDPlusID", fcLine, "Starting_NHDPlusID", field_names_fcLine)
+        
+        field_names = [f.name for f in arcpy.ListFields(fcLine, "Starting_NHDPlusID")]
+        # Create a list with tuples where each tuple contains the attribute values
+        # to pass onto the update cursor to populate fields.
+        # The Starting_NHDPlusID source attribute values are copied to all rows in the downstream trace.  
+
+        def rows_as_dicts(cursor):
+            colnames = cursor.fields
+            for row in cursor:
+                yield dict(zip(colnames, row))
+
+        fc_dict = {}
+        with arcpy.da.SearchCursor(fcLine, list(field_names)) as sc:
+            for row in rows_as_dicts(sc):
+                fc_dict.update(row)
+
+        field_range = range(len(field_names))
+
+        # Use an insert cursor to populate Starting_NHDPlusID for each row in downstream trace.
+        with arcpy.da.UpdateCursor(output_file, list(field_names)) as cursor:
+            fRange = range(len(fc_dict))
+            fVals = list(fc_dict.values())
+            for row in cursor:
+                for Index in fRange:
+                    row[Index] = fVals[Index]   
+                    cursor.updateRow(row)    
+            del cursor
+
+            
+        print("FinishedTrace "+traceGroupName)
+        
+        ## attempt to delete files in the new hydronet directory that we temporarilly added
+        delete(new_hydronet_dir)
+    except Exception as e: 
+        print(f'[!!!!!!!!! Issue processing {fcLine} --> {e} !!!!!!!!!]')
+        logger.error(f'[!!!!!!!!! Issue processing {fcLine} --> {e} !!!!!!!!!]')
+
+if __name__ == '__main__': 
+
+    print(logFile)
+    strt = datetime.now()
+    print('Starting everything now')
+
+    if os.path.exists(output_trace_dir): 
+        pass
+    else: 
+        os.makedirs(output_trace_dir)
+        os.chmod(output_trace_dir, stat.S_IWRITE)
+
+    if os.path.exists(tmp_dir): 
+        pass
+    else: 
+        os.makedirs(tmp_dir)
+    os.chmod(tmp_dir,stat.S_IWRITE)
+
+    HYDRO_NET_TRACE_Network=os.path.join(os.path.dirname(in_geometric_network), 'HydroNet_Trace')
+    
+
+    og_hydronet_dir = os.path.dirname(os.path.dirname(HYDRO_NET_TRACE_Network))
+
+    ## load in already processed
+
+    print('Grabbing all of the split flowlines')
+    ## Loop through all split flowlines.
+    fcListLines = arcpy.ListFeatureClasses("*_0",feature_type="line")
+    mappings = []
+    for fcListLine in fcListLines: 
+        mappings.append([HYDRO_NET_TRACE_Network,fcListLine,ws])
+    
+    print(f'There are {len(mappings)} split flowlines to process')
+    logger.info(f'There are {len(mappings)} split flowlines to process')
+
+    # ## Create the pool and specify the number of CPUs to use 
+    process_count = process_count_to_use
+    pool = Pool(processes=int(process_count))
+    ## Map the items in the feature_data_set dictionary to the inFDS_processing function and process
+    pool.map(preform_hazard_trace, mappings)
+    ## close the pool when finished
+    pool.close()
+    ## bring it all back together
+    pool.join()
+    
+    
+    print(f'Finished processing all hazzard layers at {datetime.now()}. Taking a break for 1 minute before moving all output generated into 1 FGDB')
+    
+    time.sleep(60)
+    # output_fgdb = create_fgdb(os.path.dirname(ws), 'AllTraceOutputs_BCM_July26_v2')
+    output_trace_files = []
+    failed_files = []
+    print(f'Moving {len(os.listdir(output_trace_dir))} files to {ws}')
+    for x in os.listdir(output_trace_dir):
+        ## Temporarily set that as the workspace
+        arcpy.env.workspace=os.path.join(output_trace_dir,x)        
+        try: 
+            to_copy=arcpy.ListFeatureClasses(wild_card = 'tempTrace_*')[0]
+            arcpy.management.CopyFeatures(to_copy, os.path.join(ws, to_copy))
+        except Exception as e: 
+            print(f'Failed to copy data in {os.path.join(output_trace_dir,x)} --> error is {e}')
+            failed_files.append(os.path.join(output_trace_dir,x))
+            pass
+        else: 
+            print(f'Moved {to_copy}')
+
+    print(f'Finished iterating all generated file geodatabases. The total number of failed copies was {len(failed_files)}')
+    for failed in failed_files: 
+        print(f'The failed files were {failed}')
+
+    end = datetime.now()
+    print(f'Total time in main is: {end-strt}')
+
